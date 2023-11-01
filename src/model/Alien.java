dc07515282b91d2479c9d0a3396a9933d9d4ff69
DanielLott4 <54151017+DanielLott4@users.noreply.github.com>
2023-11-01 11:32:23 -0700

    alien changes

--- src/model/Alien.java
+++ src/model/Alien.java
@@ -1,5 +1,33 @@
 package model;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import view_controller.SpaceInvadersGUI;
 
-public class Alien {
+public class Alien extends SpaceInvadersGUI {
+    protected ImageView imageView;
 
+    public Alien(String imageUrl) {
+        Image image = new Image(imageUrl);
+        this.imageView = new ImageView(image);
+        getChildren().add(this.imageView);
+    }
+}
+
+public class Alien1 extends Alien {
+    public Alien1() {
+        super("alien1.png");
+       
+    }
+}
+
+public class Alien2 extends Alien {
+    public Alien2() {
+        super("alien2.png");
+    }
+}
+
+public class Alien3 extends Alien {
+    public Alien3() {
+        super("alien3.png");
+    }
 }
