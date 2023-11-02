c45863ffbb432f038728e9b7b000ce23b2541947
Kavin Janarthanan <kavinjanar@gmail.com>
2023-11-01 19:30:31 -0700

    Added the spaceship and its horizontal movement

--- src/view_controller/SpaceInvadersGUI.java
+++ src/view_controller/SpaceInvadersGUI.java
@@ -1,12 +1,18 @@
 package view_controller;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import model.Alien;
 import model.Alien1;
@@ -14,9 +20,13 @@ import model.Alien2;
 import model.Alien3;
 
 public class SpaceInvadersGUI extends Application {
-	private BorderPane pane = new BorderPane();
+	private Pane pane = new Pane();
 	private TutorialPane tutorialPane = new TutorialPane();
 	private Button tutorialButton;
+	private ImageView spaceship;
+	private boolean moveLeft = false;
+	private boolean moveRight = false;
+	private final double speed = 5;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -24,17 +34,92 @@ public class SpaceInvadersGUI extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		
+		// screen parameters
+		Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+		double screenWidth = screenBounds.getWidth();
+		double screenHeight = screenBounds.getHeight();
+		
 		tutorialButton = new Button("Tutorial");
-		BorderPane.setAlignment(tutorialButton, Pos.CENTER);
-		BorderPane.setMargin(tutorialButton, new Insets(0, 0, 10, 0));
-		pane.setBottom(tutorialButton);
-
+		tutorialButton.setLayoutX(screenWidth / 2);
+		tutorialButton.setLayoutY(screenHeight * 0.9);
 		initTutorialWindow(stage);
+		
+		// space ship initialization
+		Image spaceshipImage = new Image("file:images/Spaceship.png");
+		spaceship = new ImageView(spaceshipImage);
+		System.out.println("Image loaded? " + spaceshipImage.isError());
+		
+		spaceship.setFitWidth(50);
+		spaceship.setFitHeight(60);
+		spaceship.setPreserveRatio(true);
+		spaceship.setLayoutX(screenWidth / 2);
+		spaceship.setLayoutY(screenHeight * 0.75);
+		
+		
+		pane.getChildren().add(spaceship);
+		pane.getChildren().add(tutorialButton);
+		
+		
 
 		Scene scene = new Scene(pane, 600, 400);
+		scene.setOnKeyPressed((event)->{
+			switch(event.getCode())
+			{
+			case A:
+				moveLeft = true;
+				break;
+			
+			case D:
+				moveRight = true;
+				break;
+			
+			default:
+				break;
+			}
+		});
+		scene.setOnKeyReleased((event)->{
+			switch(event.getCode())
+			{
+			case A:
+				moveLeft = false;
+				break;
+			
+			case D:
+				moveRight = false;
+				break;
+			
+			default:
+				break;
+			}
+		});
+		
+		AnimationTimer timer = new AnimationTimer() {
+			
+			@Override
+			public void handle(long arg0) {
+				// TODO Auto-generated method stub
+				int dx = 0;
+				
+				if (moveLeft) dx -= speed;
+				if (moveRight) dx += speed;
+				
+				spaceship.setLayoutX(spaceship.getLayoutX() + dx);
+				
+				double x = spaceship.getLayoutX();
+				double width = spaceship.getBoundsInLocal().getWidth();
+				
+				if (x < 0) spaceship.setLayoutX(0);
+				if (x + width > screenWidth) spaceship.setLayoutX(screenWidth - width);
+			}
+		};
+		timer.start();
+		
+		stage.setFullScreen(true);
 		stage.setTitle("Space Invaders");
 		stage.setScene(scene);
 		stage.show();
+		stage.requestFocus();
 		
 //		alienGrid(stage);
 	}
