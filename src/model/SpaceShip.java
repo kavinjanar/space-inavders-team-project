3744f79362070d37de9ea77f146d1cb4c3559089
Ethan Huang <ethanhuang@arizona.edu>
2023-11-14 09:09:59 -0700

    two player mode

--- src/model/SpaceShip.java
+++ src/model/SpaceShip.java
@@ -5,6 +5,12 @@ import javafx.scene.image.ImageView;
 
 public class SpaceShip extends ImageView {
 	private int lives;
+	private Bullet bullet;
+	private static final double bulletSpeed = 12;
+	
+	public boolean moveLeft;
+	public boolean moveRight;
+	
 	public SpaceShip(Image image)
 	{
 		super(image);
@@ -22,4 +28,18 @@ public class SpaceShip extends ImageView {
 	}
 	
 	public boolean isDestroyed() { return lives <= 0; }
+	
+	public Bullet getBullet() { return bullet; }
+	
+	public void fireBullet() {
+		bullet = new Bullet(
+				(int)(this.getLayoutX() + this.getFitWidth()/2 - 5), 
+				(int)(this.getLayoutY() - this.getFitHeight()/2), 
+				-1*(int)bulletSpeed, 
+				false);
+	}
+	
+	public void deleteBullet() {
+		bullet = null;
+	}
 }
