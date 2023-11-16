3057a501a782da0a3ad2d9d74057086cd1131a7f
Jabaar <michael.sc.evans@gmail.com>
2023-11-15 23:33:42 -0700

    Fix UFO image

--- src/model/UFO.java
+++ src/model/UFO.java
@@ -5,27 +5,25 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 public class UFO extends Pane {
-    protected ImageView imageView;
-    protected Image image1;
-    protected boolean usingFirstImage;
-    protected Image explosionImage;
-    private boolean isAlive = true;
+	protected ImageView imageView;
+	protected Image image1;
+	protected boolean usingFirstImage;
+	protected Image explosionImage;
+	private boolean isAlive = true;
 
-    public UFO() {
-        this.image1 = new Image("file:images/ufo.png");
-        
-        // Initially set the image view to the first image
-        this.imageView = new ImageView(image1);
-        this.usingFirstImage = true;
+	public UFO() {
+		this.image1 = new Image("file:images/ufo.png");
 
-        this.imageView.setFitWidth(100);
-        this.imageView.setFitHeight(40);
-        getChildren().add(this.imageView);
-        
-        this.setStyle("-fx-background-color: red;");
-    }
-        
-    public boolean isAlive() {
-        return isAlive;
-    }
+		// Initially set the image view to the first image
+		this.imageView = new ImageView(image1);
+		this.usingFirstImage = true;
+
+		this.imageView.setFitWidth(100);
+		this.imageView.setFitHeight(40);
+		getChildren().add(this.imageView);
+	}
+
+	public boolean isAlive() {
+		return isAlive;
+	}
 }
