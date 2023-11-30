f7d7217947a3ec81b0ef5671d51a8c184b725534
Jabaar <michael.sc.evans@gmail.com>
2023-11-29 23:00:53 -0700

    Add documentation

--- src/model/SoundPlayer.java
+++ src/model/SoundPlayer.java
@@ -6,6 +6,12 @@ import java.net.URI;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
+/**
+ * The SoundPlayer class provides methods for playing various sounds in the
+ * Space Invaders game. It includes functionality to play the theme song on
+ * loop, as well as individual sounds for shooting, explosions, and invader
+ * kills.
+ */
 public class SoundPlayer {
 	private MediaPlayer currentMediaPlayer;
 	private MediaPlayer shootSoundPlayer;
@@ -15,11 +21,18 @@ public class SoundPlayer {
 	private static Media explosionSound;
 	private static Media invaderKilledSound;
 
+	/**
+	 * Construct the SoundPlayer object, initializing sound files and beginning the
+	 * theme song loop.
+	 */
 	public SoundPlayer() {
 		initSounds();
 		playThemeSongLoop();
 	}
 
+	/**
+	 * Load and play the Space Invaders theme song on repeat.
+	 */
 	public void playThemeSongLoop() {
 		String path = ("sounds/SpaceInvadersTheme.mp3");
 		File file = new File(path);
@@ -36,6 +49,9 @@ public class SoundPlayer {
 		});
 	}
 
+	/**
+	 * Initialize the sounds by loading their respective files as Media objects.
+	 */
 	public void initSounds() {
 		File shootFile = new File("sounds/shoot.wav");
 		URI shootURI = shootFile.toURI();
@@ -49,21 +65,35 @@ public class SoundPlayer {
 		invaderKilledSound = new Media(invaderKilledURI.toString());
 	}
 
+	/**
+	 * Play the "shoot" sound.
+	 */
 	public void playShootSound() {
 		shootSoundPlayer = new MediaPlayer(bulletSound);
 		shootSoundPlayer.play();
 	}
 
+	/**
+	 * Play the "explosion" sound.
+	 */
 	public void playExplosionSound() {
 		explosionSoundPlayer = new MediaPlayer(explosionSound);
 		explosionSoundPlayer.play();
 	}
 
+	/**
+	 * Play the "invader killed" sound.
+	 */
 	public void playInvaderKilledSound() {
 		invaderKilledSoundPlayer = new MediaPlayer(invaderKilledSound);
 		invaderKilledSoundPlayer.play();
 	}
 
+	/**
+	 * Play a sound file given the name of a file in the sounds directory.
+	 * 
+	 * @param name The name of the file in the sounds directory.
+	 */
 	public void playSoundFile(String name) {
 		String path = ("sounds/" + name + ".mp3");
 		File file = new File(path);
