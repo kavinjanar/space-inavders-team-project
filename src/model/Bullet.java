6db8c63b89bac743e7059d93f697b2cc42e02d22
Ethan Huang <ethanhuang@arizona.edu>
2023-11-29 23:40:53 -0700

    Bullet and Shield javadoc

--- src/model/Bullet.java
+++ src/model/Bullet.java
@@ -3,13 +3,27 @@ package model;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/**
+ * Represents a bullet fired either by the player or enemy. A bullet from a
+ * player uses a different image than one from an enemy.
+ */
 public class Bullet {
 	private double speed;
 	private double startX;
 	private double startY;
 	private boolean fromEnemy;
 	private ImageView image;
-	
+
+	/**
+	 * Default constructor for a Bullet. Defines starting position, speed, and
+	 * whether or not it is from an enemy, and initializes the image based on who it
+	 * is from.
+	 * 
+	 * @param x         X coordinate of starting position
+	 * @param y         Y coordinate of starting position
+	 * @param speed     Number of pixels to move every update
+	 * @param fromEnemy Whether or not the bullet comes from an enemy
+	 */
 	public Bullet(double x, double y, double speed, boolean fromEnemy) {
 		this.startX = x;
 		this.startY = y;
@@ -24,19 +38,63 @@ public class Bullet {
 		image.setLayoutX(x);
 		image.setLayoutY(y);
 	}
-	
-	public double getStartX() { return startX; }
-	
-	public double getStartY() { return startY; }
-	
-	public double getSpeed() { return speed; }
-	
-	public boolean isFromEnemy() { return fromEnemy; }
-	
+
+	/**
+	 * Returns the X coordinate of the starting position
+	 * 
+	 * @return Starting X coordinate of the bullet
+	 */
+	public double getStartX() {
+		return startX;
+	}
+
+	/**
+	 * Returns the Y coordinate of the starting position
+	 * 
+	 * @return Starting Y coordinate of the bullet
+	 */
+	public double getStartY() {
+		return startY;
+	}
+
+	/**
+	 * Returns the bullet's speed
+	 * 
+	 * @return Speed of the bullet
+	 */
+	public double getSpeed() {
+		return speed;
+	}
+
+	/**
+	 * Whether or not the bullet was shot by an alien
+	 * 
+	 * @return true if shot by an alien, false if shot by a player
+	 */
+	public boolean isFromEnemy() {
+		return fromEnemy;
+	}
+
+	/**
+	 * Determines if the bullet is inside the rectangle defined by the parameters
+	 * 
+	 * @param x1 X coordinate of top left corner of rectangle
+	 * @param y1 Y coordinate of top left corner of rectangle
+	 * @param x2 X coordinate of bottom right corner of rectangle
+	 * @param y2 Y coordinate of bottom right corner of rectangle
+	 * @return
+	 */
 	public boolean withinBounds(double x1, double y1, double x2, double y2) {
-		return image.getLayoutX() + image.getFitWidth() / 2 >= x1 && image.getLayoutX() + image.getFitWidth() / 2 <= x2 
+		return image.getLayoutX() + image.getFitWidth() / 2 >= x1 && image.getLayoutX() + image.getFitWidth() / 2 <= x2
 				&& image.getLayoutY() + image.getFitHeight() >= y1 && image.getLayoutY() <= y2;
 	}
-	
-	public ImageView getImageView() { return image; }
+
+	/**
+	 * Returns the ImageView used by the bullet
+	 * 
+	 * @return The JavaFX ImageView representing the bullet
+	 */
+	public ImageView getImageView() {
+		return image;
+	}
 }
--- src/model/Shield.java
+++ src/model/Shield.java
@@ -3,10 +3,22 @@ package model;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/**
+ * Represents a shield in Space Invaders. Gets slowly destroyed and loses health
+ * each time it is hit, and disappears when health becomes 0.
+ */
 public class Shield extends ImageView {
 	private int health;
 	private static final int INITIAL_HEALTH = 4;
-	
+
+	/**
+	 * Constructor for the Shield class. Takes in an x and y coordinate and size,
+	 * and initializes all instance variables
+	 * 
+	 * @param x    X position on the main pane
+	 * @param y    Y position on the main pane
+	 * @param size Size in pixels of the Shield
+	 */
 	public Shield(double x, double y, int size) {
 		health = INITIAL_HEALTH;
 		updateImage();
@@ -16,8 +28,21 @@ public class Shield extends ImageView {
 		this.setFitHeight(size);
 	}
 
-	public int getHealth() { return health; }
-				
+	/**
+	 * Getter method for shield health
+	 * 
+	 * @return int Hits left until destroyed
+	 */
+	public int getHealth() {
+		return health;
+	}
+
+	/**
+	 * Called when something hits the shield and damages it
+	 * 
+	 * @return true if not destroyed and image is updated, false if shield is
+	 *         already destroyed
+	 */
 	public boolean hit() {
 		if (health == 0)
 			return false;
@@ -25,12 +50,18 @@ public class Shield extends ImageView {
 		updateImage();
 		return true;
 	}
-		
+
+	/**
+	 * Sets health back to what it was when initialized
+	 */
 	public void resetHealth() {
 		health = INITIAL_HEALTH;
 		updateImage();
 	}
-	
+
+	/**
+	 * Updates the image used based on the health of the shield
+	 */
 	private void updateImage() {
 		this.setImage(new Image("file:images/newShield" + health + ".png"));
 	}
